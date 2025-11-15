9a695ca656f5032c1dec8474de609e220cfa21a6
Rahul Chenicheri <crnambiar@gmail.com>
2025-11-15 18:54:15 +0530

    NodeJs First commit

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  secret: "bezkoder-secret-key"
+  secret: "acusep-secret-key"
 };
--- app/models/index.js
+++ app/models/index.js
@@ -1,15 +1,13 @@
 const config = require("../config/db.config.js");
+const secureEnv = require('secure-env');
+global.env = secureEnv({secret:'XZ101023FGUS'});
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  config.DB,
-  config.USER,
-  config.PASSWORD,
-  {
+  const sequelize = new Sequelize(config.DB, config.USER, global.env.PASSWORD, {
     host: config.HOST,
     dialect: config.dialect,
-    operatorsAliases: false,
-
+    operatorsAliases: 0,
+    
     pool: {
       max: config.pool.max,
       min: config.pool.min,
@@ -24,8 +22,12 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.jobstatus = require("../models/jobstatus.model.js")(sequelize, Sequelize);
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.ospatch = require("../models/ospatchreport.model.js")(sequelize, Sequelize);
+db.airportgeo = require("../models/airportgeo.model.js")(sequelize, Sequelize);
+db.airportconnect = require("../models/airportconnect.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
